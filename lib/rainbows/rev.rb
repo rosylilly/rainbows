319f6ca5e72e9705cf0a20ae5a79c3d96ab823f2
Eric Wong <normalperson@yhbt.net>
2009-10-19 14:02:33 -0700

    rev: remove Revactor-specific workaround
    This was blindly copied and pasted from the Revactor code.

--- lib/rainbows/rev.rb
+++ lib/rainbows/rev.rb
@@ -1,10 +1,6 @@
 # -*- encoding: binary -*-
 require 'rev'
 
-# workaround revactor 0.1.4 still using the old Rev::Buffer
-# ref: http://rubyforge.org/pipermail/revactor-talk/2009-October/000034.html
-defined?(Rev::Buffer) or Rev::Buffer = IO::Buffer
-
 module Rainbows
 
   # Implements a basic single-threaded event model with
