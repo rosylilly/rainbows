eb0b56d71731d2dbcfe4022b202677b9173ef81d
Eric Wong <normalperson@yhbt.net>
2009-11-04 23:59:04 -0800

    t0004: tighten up timeout test
    We've worked around trigger happy timeouts in the
    master since we track the timeout at a lower resolution
    here.

--- t/t0004-heartbeat-timeout.sh
+++ t/t0004-heartbeat-timeout.sh
@@ -31,8 +31,8 @@ t_begin "block the worker process to force it to die" && {
 	test x"$err" != x"$worker_pid"
 }
 
-t_begin "ensure timeout took 2-6 seconds" && {
-	test $elapsed -ge 2
+t_begin "ensure timeout took 3-6 seconds" && {
+	test $elapsed -ge 3
 	test $elapsed -le 6 # give it some slack in case box is bogged down
 }
 
