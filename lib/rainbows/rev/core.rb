rosylilly/rainbows

commit 145185b76dafebe5574e6a3eefd3276555c72016

rev/core: set IO::Buffer.default_node_size if possible
It can noticeably improve performance if available.

ref: http://rubyforge.org/pipermail/rev-talk/2009-November/000116.html

lib/rainbows/rev/core.rb
@@ -1,6 +1,8 @@
 # -*- encoding: binary -*-
 require 'rev'
 Rev::VERSION >= '0.3.0' or abort 'rev >= 0.3.0 is required'
+if IO::Buffer.respond_to?(:default_node_size=)
+  IO::Buffer.default_node_size = Rev::IO::INPUT_SIZE
 require 'rainbows/rev/heartbeat'
 
 module Rainbows

commit 6443cc4b87e3ba71ad661d795c3856f95a058a24

rev/core: fix stupid syntax error :<
Broken in 145185b76dafebe5574e6a3eefd3276555c72016

lib/rainbows/rev/core.rb
@@ -1,7 +1,7 @@
 # -*- encoding: binary -*-
 require 'rev'
 Rev::VERSION >= '0.3.0' or abort 'rev >= 0.3.0 is required'
-if IO::Buffer.respond_to?(:default_node_size=)
+IO::Buffer.respond_to?(:default_node_size=) and
   IO::Buffer.default_node_size = Rev::IO::INPUT_SIZE
 require 'rainbows/rev/heartbeat'
 