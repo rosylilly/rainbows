8a15fc14067e85c419a31e6d70c2491e65d88e60
Eric Wong <normalperson@yhbt.net>
2009-12-22 13:30:19 -0800

    avoid setting "rainbows.autochunk" by default
    No point in becoming the straw that causes a rehash
    since hardly anybody uses it.

--- lib/rainbows/const.rb
+++ lib/rainbows/const.rb
@@ -11,8 +11,8 @@ module Rainbows
       "SERVER_SOFTWARE" => "Rainbows! #{RAINBOWS_VERSION}",
 
       # using the Rev model, we'll automatically chunk pipe and socket objects
-      # if they're the response body
-      'rainbows.autochunk' => false,
+      # if they're the response body.  Unset by default.
+      # "rainbows.autochunk" => false,
     })
 
     CONN_CLOSE = "Connection: close\r\n"
