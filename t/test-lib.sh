08dfbae3d340114810fbd279c2ea29825d4b79e8
Eric Wong <normalperson@yhbt.net>
2010-02-18 19:33:13 -0800

    test-lib: remove needless fd closing
    too dangerous with the ready_pipe feature in Unicorn 0.96+

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -99,12 +99,6 @@ pid "$pid"
 stderr_path "$r_err"
 stdout_path "$r_out"
 
-# close my-tap-lib.sh FDs
-unless ENV['UNICORN_FD']
-  IO.for_fd(3).close rescue nil
-  IO.for_fd(4).close rescue nil
-end
-
 before_fork do |server, worker|
   # test script will block while reading from $fifo,
   # so notify the script on the first worker we spawn
