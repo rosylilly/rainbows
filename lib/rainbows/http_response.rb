77f1e141e75f27b81bef5c3bddd01948d6236ebf
Eric Wong <normalperson@yhbt.net>
2010-04-17 10:33:43 -0700

    http_response: split out header stringification code
    This will make it easier to use body#to_path if possible since
    some concurrency models like EventMachine have optimized code
    paths for serving static files.

--- lib/rainbows/http_response.rb
+++ lib/rainbows/http_response.rb
@@ -5,28 +5,30 @@ module Rainbows
 
   class HttpResponse < ::Unicorn::HttpResponse
 
+    def self.header_string(status, headers, out)
+      status = CODES[status.to_i] || status
+
+      headers.each do |key, value|
+        next if %r{\AX-Rainbows-}i =~ key
+        next if SKIP.include?(key.downcase)
+        if value =~ /\n/
+          # avoiding blank, key-only cookies with /\n+/
+          out.concat(value.split(/\n+/).map! { |v| "#{key}: #{v}\r\n" })
+        else
+          out << "#{key}: #{value}\r\n"
+        end
+      end
+
+      "HTTP/1.1 #{status}\r\n" \
+      "Date: #{Time.now.httpdate}\r\n" \
+      "Status: #{status}\r\n" \
+      "#{out.join('')}\r\n"
+    end
+
     def self.write(socket, rack_response, out = [])
       status, headers, body = rack_response
-
-      if Array === out
-        status = CODES[status.to_i] || status
-
-        headers.each do |key, value|
-          next if %r{\AX-Rainbows-}i =~ key
-          next if SKIP.include?(key.downcase)
-          if value =~ /\n/
-            # avoiding blank, key-only cookies with /\n+/
-            out.concat(value.split(/\n+/).map! { |v| "#{key}: #{v}\r\n" })
-          else
-            out << "#{key}: #{value}\r\n"
-          end
-        end
-
-        socket.write("HTTP/1.1 #{status}\r\n" \
-                     "Date: #{Time.now.httpdate}\r\n" \
-                     "Status: #{status}\r\n" \
-                     "#{out.join('')}\r\n")
-      end
+      out.instance_of?(Array) and
+        socket.write(header_string(status, headers, out))
 
       body.each { |chunk| socket.write(chunk) }
       ensure
