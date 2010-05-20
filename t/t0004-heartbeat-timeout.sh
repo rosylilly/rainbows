ac92728df5a673076e3fedd1436cef7c24e534b5
Eric Wong <normalperson@yhbt.net>
2010-05-20 03:40:49 -0700

    tests: t0004: beef up test for kills
    since we don't set maximum time boundaries, just rely on
    the logs to properly log the dead processes.

--- t/t0004-heartbeat-timeout.sh
+++ t/t0004-heartbeat-timeout.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ./test-lib.sh
 
-t_plan 9 "heartbeat/timeout test for $model"
+t_plan 12 "heartbeat/timeout test for $model"
 
 t_begin "setup and startup" && {
 	rainbows_setup $model
@@ -22,13 +22,19 @@ t_begin "sleep for a bit, ensure worker PID does not change" && {
 }
 
 t_begin "block the worker process to force it to die" && {
+	rm $ok
 	t0=$(date +%s)
-	err="$(curl -sSf http://$listen/block-forever 2>&1 || :)"
+	err="$(curl -sSf http://$listen/block-forever 2>&1 || > $ok)"
 	t1=$(date +%s)
 	elapsed=$(($t1 - $t0))
 	t_info "elapsed=$elapsed err=$err"
 	test x"$err" != x"Should never get here"
 	test x"$err" != x"$worker_pid"
+}
+
+t_begin "ensure worker was killed" && {
+	test -e $ok
+	test 1 -eq $(grep timeout $r_err | grep killing | wc -l)
 }
 
 t_begin "ensure timeout took at least 3 seconds" && {
@@ -44,6 +50,10 @@ t_begin "we get a fresh new worker process" && {
 	test $new_worker_pid -ne $worker_pid
 }
 
+t_begin "truncate the server error log" && {
+	> $r_err
+}
+
 t_begin "SIGSTOP and SIGCONT on rainbows master does not kill worker" && {
 	kill -STOP $rainbows_pid
 	sleep 4
@@ -53,8 +63,10 @@ t_begin "SIGSTOP and SIGCONT on rainbows master does not kill worker" && {
 }
 
 t_begin "stop server" && {
-	kill $rainbows_pid
+	kill -QUIT $rainbows_pid
 }
+
+t_begin "check stderr" && check_stderr
 
 dbgcat r_err
 
