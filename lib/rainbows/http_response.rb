43745f26e201655f98351684e1d357cbd8c15b05
Eric Wong <normalperson@yhbt.net>
2010-06-27 23:36:51 +0000

    http_response: cleanup documentation
    No point in documenting our internals and overwhelming
    users.

--- lib/rainbows/http_response.rb
+++ lib/rainbows/http_response.rb
@@ -1,38 +1,37 @@
 # -*- encoding: binary -*-
-require 'time'
+require 'time' # for Time#httpdate
 
-module Rainbows
+# :stopdoc:
+class Rainbows::HttpResponse < ::Unicorn::HttpResponse
 
-  class HttpResponse < ::Unicorn::HttpResponse
+  def self.header_string(status, headers, out)
+    status = CODES[status.to_i] || status
 
-    def self.header_string(status, headers, out)
-      status = CODES[status.to_i] || status
-
-      headers.each do |key, value|
-        next if %r{\AX-Rainbows-}i =~ key
-        next if SKIP.include?(key.downcase)
-        if value =~ /\n/
-          # avoiding blank, key-only cookies with /\n+/
-          out.concat(value.split(/\n+/).map! { |v| "#{key}: #{v}\r\n" })
-        else
-          out << "#{key}: #{value}\r\n"
-        end
+    headers.each do |key, value|
+      next if %r{\AX-Rainbows-}i =~ key
+      next if SKIP.include?(key.downcase)
+      if value =~ /\n/
+        # avoiding blank, key-only cookies with /\n+/
+        out.concat(value.split(/\n+/).map! { |v| "#{key}: #{v}\r\n" })
+      else
+        out << "#{key}: #{value}\r\n"
       end
-
-      "HTTP/1.1 #{status}\r\n" \
-      "Date: #{Time.now.httpdate}\r\n" \
-      "Status: #{status}\r\n" \
-      "#{out.join('')}\r\n"
     end
 
-    def self.write(socket, rack_response, out = [])
-      status, headers, body = rack_response
-      out.instance_of?(Array) and
-        socket.write(header_string(status, headers, out))
+    "HTTP/1.1 #{status}\r\n" \
+    "Date: #{Time.now.httpdate}\r\n" \
+    "Status: #{status}\r\n" \
+    "#{out.join('')}\r\n"
+  end
 
-      body.each { |chunk| socket.write(chunk) }
-      ensure
-        body.respond_to?(:close) and body.close
-    end
+  def self.write(socket, rack_response, out = [])
+    status, headers, body = rack_response
+    out.instance_of?(Array) and
+      socket.write(header_string(status, headers, out))
+
+    body.each { |chunk| socket.write(chunk) }
+    ensure
+      body.respond_to?(:close) and body.close
   end
 end
+# :startdoc:
