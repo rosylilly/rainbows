e1c10949271a9cd3cbad86d05ce0436f2e79bc65
Eric Wong <normalperson@yhbt.net>
2010-07-09 08:30:15 +0000

    http_response: allow Cramp 0.11 to send proper WS headers
    Cramp needs to override our normal header sending for
    (old) WebSockets connections.

--- lib/rainbows/http_response.rb
+++ lib/rainbows/http_response.rb
@@ -1,10 +1,27 @@
 # -*- encoding: binary -*-
 # deprecated, use Rainbows::Response instead
-# Cramp 0.11 relies on this
+# Cramp 0.11 relies on this, and is only activated by Cramp
 # :enddoc:
-class Rainbows::HttpResponse
-  class << self
-    include Rainbows::Response
-    alias write write_response
+if defined?(Cramp) && defined?(Rainbows::EventMachine::Client)
+  class Rainbows::HttpResponse
+    class << self
+      include Rainbows::Response
+      alias write write_response
+    end
+  end
+
+  module Rainbows::EventMachine::CrampSocket
+    def write_header(_, response, out)
+      if websocket?
+        write web_socket_upgrade_data
+        web_socket_handshake!
+        out = nil # disable response headers
+      end
+      super(self, response, out)
+    end
+  end
+
+  class Rainbows::EventMachine::Client
+    include Rainbows::EventMachine::CrampSocket
   end
 end
