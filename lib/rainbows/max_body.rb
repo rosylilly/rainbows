3cee07d750f678af92318c14110c803be3f9b97f
Eric Wong <normalperson@yhbt.net>
2010-11-19 10:19:44 +0000

    max_body: do not enable for RevThread* models
    Those already use CapInput, just like the rest of the evented
    Rainbows! world.

--- lib/rainbows/max_body.rb
+++ lib/rainbows/max_body.rb
@@ -53,7 +53,7 @@ class Rainbows::MaxBody
   def self.setup # :nodoc:
     Rainbows.max_bytes or return
     case Rainbows::G.server.use
-    when :Rev, :EventMachine, :NeverBlock
+    when :Rev, :EventMachine, :NeverBlock, :RevThreadSpawn, :RevThreadPool
       return
     end
 
