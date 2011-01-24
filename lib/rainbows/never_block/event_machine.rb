85ccfbc75abc78f977583db6303e3fb270fd2ec9
Eric Wong <normalperson@yhbt.net>
2011-01-24 13:20:23 -0800

    neverblock: fix app_call under 1.8.7
    super doesn't seem to capture arguments inside a block
    under 1.8.7 :<

--- lib/rainbows/never_block/event_machine.rb
+++ lib/rainbows/never_block/event_machine.rb
@@ -4,7 +4,7 @@ class Rainbows::NeverBlock::Client < Rainbows::EventMachine::Client
   def app_call input
     POOL.spawn do
       begin
-        super
+        super input
       rescue => e
         handle_error(e)
       end
