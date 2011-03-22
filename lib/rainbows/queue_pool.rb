777beb3e27785d2da2865cb5fc0d43f1c158cb5e
Eric Wong <normalperson@yhbt.net>
2011-03-22 11:08:31 -0700

    queue_pool: switch to ivars to protect internals

--- lib/rainbows/queue_pool.rb
+++ lib/rainbows/queue_pool.rb
@@ -5,24 +5,26 @@ require 'thread'
 # Thread pool class based on pulling off a single Ruby Queue.
 # This is NOT used for the ThreadPool class, since that class does not
 # need a userspace Queue.
-class Rainbows::QueuePool < Struct.new(:queue, :threads)
+class Rainbows::QueuePool
+  attr_reader :queue
+
   def initialize(size = 20)
     q = Queue.new
-    self.threads = (1..size).map do
+    @threads = (1..size).map do
       Thread.new do
         while job = q.shift
           yield job
         end
       end
     end
-    self.queue = q
+    @queue = q
   end
 
   def quit!
-    threads.each { |_| queue << nil }
-    threads.delete_if do |t|
+    @threads.each { |_| @queue << nil }
+    @threads.delete_if do |t|
       Rainbows.tick
       t.alive? ? t.join(0.01) : true
-    end until threads.empty?
+    end until @threads.empty?
   end
 end
