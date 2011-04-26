e3c8152e016fe29f76a996b8802df7158c081db0
Eric Wong <normalperson@yhbt.net>
2011-04-26 12:56:20 -0700

    stream_file: hide internals
    No need to expose things that don't need exposing.

--- lib/rainbows/stream_file.rb
+++ lib/rainbows/stream_file.rb
@@ -5,10 +5,17 @@
 # models.  We always maintain our own file offsets in userspace because
 # because sendfile() implementations offer pread()-like idempotency for
 # concurrency (multiple clients can read the same underlying file handle).
-class Rainbows::StreamFile < Struct.new(:offset, :count, :to_io, :body)
+class Rainbows::StreamFile
+  attr_reader :to_io
+  attr_accessor :offset, :count
+
+  def initialize(offset, count, io, body)
+    @offset, @count, @to_io, @body = offset, count, io, body
+  end
+
   def close
-    body.close if body.respond_to?(:close)
-    to_io.close unless to_io.closed?
-    self.to_io = nil
+    @body.close if @body.respond_to?(:close)
+    @to_io.close unless @to_io.closed?
+    @to_io = nil
   end
 end
