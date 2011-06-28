rosylilly/rainbows

commit 905f0ff393629ddb4d70e3dc221b016128c47415

require kgio 2.5 for kgio_wait_readable(timeout)
Since kgio_wait_*able in kgio 2.5 takes an optional timeout
argument, we no longer have to load the extra "io/wait" module.
This saves us a small amount of some memory and also removes the
extra ioctl(FIONREAD) syscall IO#wait enforces.

Like IO#wait in Ruby 1.9.3dev, kgio_wait_readable may use
ppoll() to wait on high-numbered file descriptors as efficiently
as it waits on low-numbered descriptors.

lib/rainbows/client.rb
@@ -1,6 +1,5 @@
 # -*- encoding: binary -*-
 # :enddoc:
-require "io/wait"
 
 # this class is used for most synchronous concurrency models
 class Rainbows::Client < Kgio::Socket
@@ -11,10 +10,6 @@ class Rainbows::Client < Kgio::Socket
     Time.now + KEEPALIVE_TIMEOUT
   end
 
-  def kgio_wait_readable
-    wait KEEPALIVE_TIMEOUT
-  end
-
   # used for reading headers (respecting keepalive_timeout)
   def timed_read(buf)
     expire = nil
@@ -23,7 +18,7 @@ class Rainbows::Client < Kgio::Socket
       when :wait_readable
         return if expire && expire < Time.now
         expire ||= read_expire
-        kgio_wait_readable
+        kgio_wait_readable(KEEPALIVE_TIMEOUT)
       else
         return rv
       end

lib/rainbows/fiber/coolio/methods.rb
@@ -28,7 +28,7 @@ module Rainbows::Fiber::Coolio::Methods
     @w.disable
   end
 
-  def kgio_wait_readable
+  def kgio_wait_readable(timeout = nil)
     @r = Watcher.new(self, :r) unless defined?(@r)
     @r.enable unless @r.enabled?
     Fiber.yield

lib/rainbows/fiber/io/methods.rb
@@ -29,7 +29,7 @@ module Rainbows::Fiber::IO::Methods
     super
   end
 
-  def kgio_wait_readable
+  def kgio_wait_readable(timeout = nil)
     fd = fileno
     @f = Fiber.current
     RD[fd] = self

rainbows.gemspec
@@ -24,8 +24,8 @@ Gem::Specification.new do |s|
   # we want a newer Rack for a valid HeaderHash#each
   s.add_dependency(%q<rack>, ['~> 1.1'])
 
-  # kgio has some fixes for MRI 1.9.3dev that affect us
-  s.add_dependency(%q<kgio>, ['~> 2.4'])
+  # kgio 2.5 has kgio_wait_* methods that take optional timeout args
+  s.add_dependency(%q<kgio>, ['~> 2.5'])
 
   # we need Unicorn for the HTTP parser and process management
   s.add_dependency(%q<unicorn>, ["~> 4.0"])

commit 65f251070ce9b61a9049cb413fccffdfa11e7a70

Fix Fiber* and WriterThread* breakage from removing io/wait
This fixes up breakage introduced in commit
905f0ff393629ddb4d70e3dc221b016128c47415 to switch to
kgio for timed, synchronous waiting.

lib/rainbows/client.rb
@@ -4,26 +4,6 @@
 # this class is used for most synchronous concurrency models
 class Rainbows::Client < Kgio::Socket
   include Rainbows::ProcessClient
-  Rainbows.config!(self, :keepalive_timeout)
-
-  def read_expire
-    Time.now + KEEPALIVE_TIMEOUT
-  end
-
-  # used for reading headers (respecting keepalive_timeout)
-  def timed_read(buf)
-    expire = nil
-    begin
-      case rv = kgio_tryread(CLIENT_HEADER_BUFFER_SIZE, buf)
-      when :wait_readable
-        return if expire && expire < Time.now
-        expire ||= read_expire
-        kgio_wait_readable(KEEPALIVE_TIMEOUT)
-      else
-        return rv
-      end
-    end while true
-  end
 
   alias write kgio_write
 end

lib/rainbows/process_client.rb
@@ -7,7 +7,26 @@ module Rainbows::ProcessClient
   NULL_IO = Unicorn::HttpRequest::NULL_IO
   RACK_INPUT = Unicorn::HttpRequest::RACK_INPUT
   IC = Unicorn::HttpRequest.input_class
-  Rainbows.config!(self, :client_header_buffer_size)
+  Rainbows.config!(self, :client_header_buffer_size, :keepalive_timeout)
+
+  def read_expire
+    Time.now + KEEPALIVE_TIMEOUT
+  end
+
+  # used for reading headers (respecting keepalive_timeout)
+  def timed_read(buf)
+    expire = nil
+    begin
+      case rv = kgio_tryread(CLIENT_HEADER_BUFFER_SIZE, buf)
+      when :wait_readable
+        return if expire && expire < Time.now
+        expire ||= read_expire
+        kgio_wait_readable(KEEPALIVE_TIMEOUT)
+      else
+        return rv
+      end
+    end while true
+  end
 
   def process_loop
     @hp = hp = Rainbows::HttpParser.new

lib/rainbows/socket_proxy.rb
@@ -18,6 +18,14 @@ module Rainbows::SocketProxy
     to_io.kgio_trywrite(buf)
   end
 
+  def kgio_tryread(size, buf = "")
+    to_io.kgio_tryread(size, buf)
+  end
+
+  def kgio_wait_readable(timeout = nil)
+    to_io.kgio_wait_readable(timeout)
+  end
+
   def timed_read(buf)
     to_io.timed_read(buf)
   end