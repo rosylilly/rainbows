9573d9bc6c63aced28300a9d7346b5575eed8b7d
Eric Wong <normalperson@yhbt.net>
2012-05-08 23:18:41 +0000

    tests: require sleepy_penguin 3.1.0 or later
    We'll be making the XEpollThreadPool users depend on this, too.

--- t/test_isolate.rb
+++ t/test_isolate.rb
@@ -38,7 +38,7 @@ Isolate.now!(opts) do
   end
 
   if RUBY_PLATFORM =~ /linux/
-    gem 'sleepy_penguin', '3.0.1'
+    gem 'sleepy_penguin', '3.1.0'
 
     # is 2.6.32 new enough?
     gem 'io_splice', '4.1.1' if `uname -r`.strip > '2.6.32'
